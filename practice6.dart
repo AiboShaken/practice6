55843e91116eb26d678b9148d23507295d6c7a21
AiboShaken <aiboshaken@gmail.com>
2023-05-27 18:01:50 +0600

    first commit

---
+++ practice6.dart
@@ -0,0 +1,158 @@
+void main() {
+  //task1
+  // int a = 5;
+  // double b = 5.126;
+  // print(a + b);
+
+  // int a1 = 5;
+  // double b1 = 5.126;
+  // print(a1 - b1);
+
+  // int a2 = 5;
+  // double b2 = 5.126;
+  // print(a2 ~/ b2);
+
+  // int a3 = 5;
+  // double b3 = 5.126;
+  // print(a * b);
+
+  //task2
+  // int plus = 5 + 2;
+  // print('5 + 2 = $plus');
+  // int subtraction = 5 - 2;
+  // print('5 - 2 = $subtraction');
+  // int multiplication = 132 * 123;
+  // print('132 * 123 = $multiplication');
+  // double division1 = 78 / 2;
+  // print('78 / 2 = $division1');
+  // double division2 = 78 / 2.5;
+  // print('78 / 2.5 = $division2');
+  // int multiplicationZero = 0 * 5;
+  // print('0 * 5 = $multiplicationZero');
+
+  //task4
+  // print('enter your name');
+  // String name = stdin.readLineSync()!;
+
+  // print('enter your age');
+  // String ageInput = stdin.readLineSync()!;
+
+  // print('your $name and your $ageInput');
+
+// task5
+// print('enter your number:');
+//   String number = stdin.readLineSync()??'';
+
+//   if (number.length == 13) {
+//     print('The entered number is valid.');
+//   } else {
+//     print('The entered number is not valid.');
+//   }
+
+// task6
+// bool isRaining = false;
+// String takeUmbrella = isRaining ? "take an umbrella with you" :
+// "not needed an umbrella";
+// print(takeUmbrella);
+
+//task7
+  // int num = 7;
+
+  // if (num > 3) {
+  //   num += 10;
+  // } else {
+  //   num -= 10;
+  // }
+  // print("Total number: $num");
+
+  //task8
+  // int number = 3;
+
+  // if (number < 7) {
+  //   print("Yes");
+  // } else if (number > 10) {
+  //   print("No");
+  // } else if (number == 9) {
+  //   print("Error");
+  // }
+
+// task9
+// int num1 = 20;
+// int num2 = 30;
+// int maxNum = num1 > num2 ? num1 : num2;
+// print("highest number: $maxNum ");
+
+// task10
+  // int num1 = 70;
+  // int num2 = 30;
+  // String result = (num1 + num2 == 100) ? "Yes" : "No";
+  // print(result);
+
+  // task11
+  // print("Enter month number: ");
+  // int month = int.parse(stdin.readLineSync()!);
+  // String season = "";
+
+  // if (month >= 1 && month <= 12) {
+  //   if (month >= 3 && month <= 5) {
+  //     season = "spring";
+  //   } else if (month >= 6 && month <= 8) {
+  //     season = "summer";
+  //   } else if (month >= 9 && month <= 11) {
+  //     season = "autumn";
+  //   } else {
+  //     season = "winter";
+  //   }
+
+  //   print("season: $season");
+  // } else {
+  //   print("invalid month number, please enter number from 1 to 12.");
+  // }\
+
+  // task12
+  // int num1 = 50;
+  // int num2 = 60;
+  // int num3 = 10;
+
+  // int min = num1;
+
+  // if (num2 < min) {
+  //   min = num2;
+  // }
+  // if (num3 < min) {
+  //   min = num3;
+  // }
+  // print("Smallest number: $min");
+
+  // task13
+
+  // task14
+  // List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+  // for (int i = 0; i < a.length; i++) {
+  //   if (a[i] < 5) {
+  //     print("element by index $i: ${a[i]}");
+  //   }
+  // }
+
+  // task15
+  // int a = 5;
+
+  // for (int i = 1; i <= a; i++) {
+  //   String stars = '';
+  //   for (int j = 1; j <= i; j++) {
+  //     stars += '* ';
+  //   }
+  //   print(stars);
+  // }
+
+  // task16
+  // for (int i = 1; i <= 5; i++) {
+  //   print(i);
+  // }
+
+  // task17
+  //   for (int i = 5; i >= 1; i--) {
+  //     print(i);
+  //   }
+}
